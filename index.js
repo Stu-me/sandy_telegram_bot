b577d8739228930bc876f6d9aa577fa96378a8fe
Stu-me <stusundram@gmail.com>
2026-03-04 15:34:55 +0530

    telegram bot made global

---
+++ index.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+const axios = require('axios');
+const dotenv = require
+
+app.use(bodyParser.json());
+
+app.use(
+    bodyParser.urlencoded({extended:true})
+)
+
+app.post("/new-message",(req,res)=>{
+    const {message} = req.body;
+
+    if(!message || message.txt.toLower().indexOf("marco")<0){
+        // if empty or no macro found in the lowercased of the message
+        return res.json({
+            msg:" the impotant keyword is missing "
+        })
+    }
+    axios
+    .post("https:api.telegram.org/bot",{
+        chat_id:message.chat.id,
+        text:"Pollo",
+    })
+    .then((response)=>{
+        // we get here if the message was successfully posted
+        console.log("Message was posted ");
+        res.end("ok");
+    })
+    .catch((err)=>{
+        console.log("Error:",err);
+        res.end("Error:",err)
+    })
+
+})
+
+app.listen(8080,(req,res)=>{
+    console.log("server started");
+})
