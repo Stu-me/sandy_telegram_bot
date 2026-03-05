85a178a8eaf0a12c33a28059b3043f0612a93fae
Stu-me <stusundram@gmail.com>
2026-03-05 10:26:55 +0530

    one more try to correct

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
-const axios = require('axios');
- require('dotenv').config();
+// 
 
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({extended:true}))
-console.log("https://api.telegram.org/bot"+process.env.TELEGRAM_API_TOKEN+"/sendMessage");
+const TOKEN = process.env.TELEGRAM_API_TOKEN;
 
-app.post("/new-message",(req,res)=>{
-    const {message} = req.body;
-    // if(!message || !message.text || message.text.toLowerCase().indexOf("marco")<0){
-    //     // if empty or no macro found in the lowercased of the message
-    //     return res.json({
-    //         msg:" the impotant keyword is missing "
-    //     })
-    // }
-    axios
-    .post("https://api.telegram.org/bot"+process.env.TELEGRAM_API_TOKEN+"/sendMessage",{
-        chat_id:message.chat.id,
-        text:"Pollo",
-    })
-    .then((response)=>{
-        // we get here if the message was successfully posted
-        console.log("Message was posted ");
-        res.end("ok");
-    })
-    .catch((err)=>{
-        console.log("Error:",err);
-        res.end("Error:")
-    })
+if (!TOKEN) {
+  throw new Error("Telegram token missing in .env");
+}
 
-})
+app.post("/new-message", async (req, res) => {
 
-app.listen(8080,()=>{
-    console.log("server started");
-})
+  console.log(JSON.stringify(req.body,null,2));
+
+  if (!req.body.message) {
+    return res.end("No message object");
+  }
+
+  const chatId = req.body.message.chat.id;
+
+  try {
+
+    await axios.post(
+      `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+      {
+        chat_id: chatId,
+        text: "Pollo"
+      },
+      { timeout: 5000 }
+    );
+
+    console.log("Message sent");
+
+    res.end("ok");
+
+  } catch (err) {
+
+    console.log("Telegram error:", err.response?.data || err.message);
+
+    res.end("error");
+
+  }
+
+});
+
+app.listen(8080, () => {
+  console.log("Server running on 8080");
+});
